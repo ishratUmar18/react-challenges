55b9b371f20b1aa096e45c2530b125b403f0a377
Ishrat <47534248+ishratUmar18@users.noreply.github.com>
2022-08-29 12:38:12 +0500

    Update conditional-rendering.js

--- conditional-rendering.js
+++ conditional-rendering.js
@@ -12,8 +12,7 @@ const [messages, setMessages] = React.useState(["a"])
             {
                 messages.length === 0 ?
                 <h1>You're all caught up!</h1> :
-                <h1>You have {messages.length} unread 
-                {messages.length > 1 ? "messages" : "message"}</h1>
+                <h1>You have {messages.length} unread {messages.length > 1 ? "messages" : "message"}</h1>
             }
         </div>
     )
